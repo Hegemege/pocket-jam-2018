cefe1ec2cfd070489cbdf3647ccb2c0d68d02555
Henri Niva <hegemege@gmail.com>
2018-09-11 17:32:56 +0300

    Add camera script, make player sprite look towards camera

--- Assets/Scripts/AIAgentController.cs
+++ Assets/Scripts/AIAgentController.cs
@@ -8,9 +8,25 @@ public class AIAgentController : MonoBehaviour
     protected NavMeshAgent _agent;
     protected int _selectedStationIndex; // TODO: Vaihda enumiksi
 
-    void Awake()
+    protected SpriteRenderer[] _sprites;
+
+    protected virtual void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _sprites = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    protected virtual void Update()
+    {
+        if (GameManager.Instance.WorldCamera == null) return;
+
+        var cameraForwardOnZ = Vector3.ProjectOnPlane(GameManager.Instance.WorldCamera.transform.forward, Vector3.up);
+
+        for (var i = 0; i < _sprites.Length; i++)
+        {
+            _sprites[i].transform.rotation =
+                Quaternion.LookRotation(cameraForwardOnZ, Vector3.up);
+        }
     }
 
     public void SetMoveTarget(Vector3 target)
---
+++ Assets/Scripts/WorldCameraController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCameraController : MonoBehaviour
+{
+    void Awake()
+    {
+        GameManager.Instance.WorldCamera = this;
+    }
+}
