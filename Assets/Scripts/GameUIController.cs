c5586891dee16c7f57d279c7d1c84e6e84d627f5
Henri Niva <hegemege@gmail.com>
2018-09-11 13:58:04 +0300

    Scalable menu

---
+++ Assets/Scripts/GameUIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour
+{
+    public Image StockGraph;
+    public Text StockNameText;
+
+    void Awake()
+    {
+
+    }
+
+    public void PreviousStation()
+    {
+
+    }
+
+    public void NextStation()
+    {
+
+    }
+}
