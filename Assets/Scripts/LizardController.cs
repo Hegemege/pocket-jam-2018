fcb1eff314b898779b9b39d3a3b0019844bd9edb
Henri Niva <henri.niva@aalto.fi>
2018-09-11 23:57:40 +0300

    Start lizard animation on random frame

--- Assets/Scripts/LizardController.cs
+++ Assets/Scripts/LizardController.cs
@@ -7,6 +7,15 @@ public class LizardController : AIAgentController
 {
     public StockType PreferredStockType;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Animator anim = GetComponentInChildren<Animator>();
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+        anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+
     protected override void ReachedTarget()
     {
         // TODO: update station lizard count?
