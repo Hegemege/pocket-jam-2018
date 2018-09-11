55576f56f60faaddad2c95f8e62c8804a7797946
Henri Niva <hegemege@gmail.com>
2018-09-11 19:35:53 +0300

    Stuff

--- Assets/Scripts/WorldCameraController.cs
+++ Assets/Scripts/WorldCameraController.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class WorldCameraController : MonoBehaviour
 {
+    private Vector3 _initialPosition;
+
     void Awake()
     {
         GameManager.Instance.WorldCamera = this;
+        _initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (!GameManager.Instance.PlayerController) return;
+
+        transform.position = _initialPosition + GameManager.Instance.PlayerController.transform.position;
     }
 }
