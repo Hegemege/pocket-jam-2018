bb38b5137e0291f36140f3ecad8f320eae146979
dschhhhh <hannu.koivuranta@gmail.com>
2018-09-12 12:34:00 +0300

    Title Scene graph implementations

--- Assets/Scripts/RotatorGraph.cs
+++ Assets/Scripts/RotatorGraph.cs
@@ -1,37 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class RotatorTitle : MonoBehaviour {
+public class RotatorGraph : MonoBehaviour
+{
 
-    // Use this for initialization
+    private bool dirRight = true;
+    public float speed = 2.0f;
 
+    void Update()
+    {
+        if (dirRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        else
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
 
+        if (transform.position.x >= 4.0f)
+        {
+            dirRight = false;
+        }
 
-    // User Inputs
-    public float speed;
-    public float amplitude = 0.5f;
-    public float frequency = 1f;
-
-    // Position Storage Variables
-    Vector3 posOffset = new Vector3();
-    Vector3 tempPos = new Vector3();
-
-    void Start () {
-        // Store the starting position & rotation of the object
-        posOffset = transform.position;
-
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(Vector2.right, 45 * Time.deltaTime * speed);
-
-        // Float up/down with a Sin()
-        tempPos = posOffset;
-        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
-
-        transform.position = tempPos;
-
+        if (transform.position.x <= -4)
+        {
+            dirRight = true;
+        }
     }
 }
